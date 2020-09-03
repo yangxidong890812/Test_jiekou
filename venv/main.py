b9827264061d529e289137d870d764dd00124ba8
Your Name <you@example.com>
2020-09-03 13:36:37 +0800

    testjiekou

---
+++ venv/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*
+
+from Interface_test_automation.case.Monitortest import MonitorTestCase
+
+if __name__ == '__main__':
+    app = MonitorTestCase()
+    app.runAllCase("新监控后台")
